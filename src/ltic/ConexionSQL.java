acc49894e6e4bf91b2ec3d38c63b81c379d534f4
JuanSe Jativa <juanse417@hotmail.com>
2014-09-23 14:31:26 -0500

    Commit desde NetBeans

---
+++ src/ltic/ConexionSQL.java
@@ -0,0 +1,47 @@
+
+package ltic;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Universidad
+ */
+public class ConexionSQL {
+      
+    public static Connection GetConnection()
+    {
+        Connection conexion=null;
+        String bd = "ltic";
+        String login = "ltic";
+        String password = "abc123";
+        String url = "jdbc:mysql://localhost/" + bd;
+        //Prueba y yo tbn nunca!!!!! 
+      
+        try
+        {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, login, password);
+        }
+        catch(ClassNotFoundException ex)
+        {
+            JOptionPane.showMessageDialog(null, ex, "Error1 en la Conexión con la BD "+ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion=null;
+        }
+        catch(SQLException ex)
+        {
+            JOptionPane.showMessageDialog(null, ex, "Error2 en la Conexión con la BD "+ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion=null;
+        }
+        catch(Exception ex)
+        {
+            JOptionPane.showMessageDialog(null, ex, "Error3 en la Conexión con la BD "+ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion=null;
+        }
+        finally
+        {
+            return conexion;
+        }
+    }
+}
