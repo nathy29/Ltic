3dd308ada011f05ff55e37796da234ba609546ce
Nathy <nathalysoledad@hotmail.com>
2014-09-25 11:04:10 -0500

    eVENTS AND FOCUSLOST EN RACKS

--- src/ltic/LticF.java
+++ src/ltic/LticF.java
@@ -56,8 +56,8 @@ public class LticF extends javax.swing.JFrame {
         MarcaRcakText = new javax.swing.JTextField();
         GuardarRackBoton = new javax.swing.JButton();
         Seguridad = new javax.swing.JPanel();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
+        SiSeguridadRadio = new javax.swing.JRadioButton();
+        NOSeguridadRadio = new javax.swing.JRadioButton();
         EliminarRackBoton = new javax.swing.JButton();
         CancelarRackBoton = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
@@ -202,6 +202,11 @@ public class LticF extends javax.swing.JFrame {
         CoRackLab.setText("Código:");
 
         CoRackText.setColumns(15);
+        CoRackText.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                CoRackTextFocusLost(evt);
+            }
+        });
 
         CoLaboratorioLab.setText("Laboratorio:");
 
@@ -212,12 +217,17 @@ public class LticF extends javax.swing.JFrame {
         MarcaRcakText.setColumns(15);
 
         GuardarRackBoton.setText("Guardar");
+        GuardarRackBoton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                GuardarRackBotonActionPerformed(evt);
+            }
+        });
 
-        gruposeguridad.add(jRadioButton1);
-        jRadioButton1.setText("SI");
+        gruposeguridad.add(SiSeguridadRadio);
+        SiSeguridadRadio.setText("SI");
 
-        gruposeguridad.add(jRadioButton2);
-        jRadioButton2.setText("NO");
+        gruposeguridad.add(NOSeguridadRadio);
+        NOSeguridadRadio.setText("NO");
 
         javax.swing.GroupLayout SeguridadLayout = new javax.swing.GroupLayout(Seguridad);
         Seguridad.setLayout(SeguridadLayout);
@@ -225,9 +235,9 @@ public class LticF extends javax.swing.JFrame {
             SeguridadLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(SeguridadLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jRadioButton1)
+                .addComponent(SiSeguridadRadio)
                 .addGap(18, 18, 18)
-                .addComponent(jRadioButton2)
+                .addComponent(NOSeguridadRadio)
                 .addContainerGap(41, Short.MAX_VALUE))
         );
         SeguridadLayout.setVerticalGroup(
@@ -235,13 +245,23 @@ public class LticF extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, SeguridadLayout.createSequentialGroup()
                 .addGap(0, 10, Short.MAX_VALUE)
                 .addGroup(SeguridadLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jRadioButton1)
-                    .addComponent(jRadioButton2)))
+                    .addComponent(SiSeguridadRadio)
+                    .addComponent(NOSeguridadRadio)))
         );
 
         EliminarRackBoton.setText("Eliminar");
+        EliminarRackBoton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                EliminarRackBotonActionPerformed(evt);
+            }
+        });
 
         CancelarRackBoton.setText("Cancelar");
+        CancelarRackBoton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CancelarRackBotonActionPerformed(evt);
+            }
+        });
 
         jLabel3.setText("Seguridad:");
 
@@ -755,6 +775,7 @@ public class LticF extends javax.swing.JFrame {
          {
            Statement st = miConexion.createStatement();
             ResultSet rs = st.executeQuery("SELECT CODIGOLAB FROM labortorio"); 
+            CoLaboCombo.addItem(" ");
             while (rs.next())
             {
                CoLaboCombo.addItem(rs.getString("CODIGOLAB"));
@@ -938,6 +959,50 @@ public class LticF extends javax.swing.JFrame {
         //Aqui va lo de elimar el grupo:
     }//GEN-LAST:event_eliminarPuertoActionPerformed
 
+    private void CoRackTextFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_CoRackTextFocusLost
+        // TODO add your handling code here:
+        GuardarRackBoton.setEnabled(true);
+        
+        Connection miConexion;
+          miConexion=coneccion.GetConnection();
+         try
+         {
+           Statement st = miConexion.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM Rack where CodigoRack='"+CoRackText.getText()+"'"); 
+            if (rs.next())
+            {
+               EliminarRackBoton.setEnabled(true);
+               CoRackText.setEnabled(false);
+               MarcaRcakText.setText(rs.getString("MARCARACK"));
+               CoLaboCombo.setSelectedItem(rs.getString("CODIGOLAB"));
+               if(rs.getInt("SEGURIDADRACK")==1)
+                    SiSeguridadRadio.setSelected(true);
+               else
+                   NOSeguridadRadio.setSelected(true);
+            }
+            rs.close();
+          }
+                catch( Exception e ){
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+                }
+
+
+
+    }//GEN-LAST:event_CoRackTextFocusLost
+
+    private void GuardarRackBotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_GuardarRackBotonActionPerformed
+        //AQUI VA GUARDAR RACK
+    }//GEN-LAST:event_GuardarRackBotonActionPerformed
+
+    private void EliminarRackBotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EliminarRackBotonActionPerformed
+      // AQUI VA ELIMINAR RACK
+    }//GEN-LAST:event_EliminarRackBotonActionPerformed
+
+    private void CancelarRackBotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CancelarRackBotonActionPerformed
+        // AQUI VA CANCELAR RACK
+    }//GEN-LAST:event_CancelarRackBotonActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -983,7 +1048,9 @@ public class LticF extends javax.swing.JFrame {
     private javax.swing.JButton GuardarRackBoton;
     private javax.swing.JLabel MarcaRackLab;
     private javax.swing.JTextField MarcaRcakText;
+    private javax.swing.JRadioButton NOSeguridadRadio;
     private javax.swing.JPanel Seguridad;
+    private javax.swing.JRadioButton SiSeguridadRadio;
     private javax.swing.JTextField buscarpuertotf;
     private javax.swing.JTextField buscarswitchtf;
     private javax.swing.JLabel codigoPuertoLabel;
@@ -1017,8 +1084,6 @@ public class LticF extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem6;
     private javax.swing.JMenuItem jMenuItem7;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
